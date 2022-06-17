d20119ece6b215ff0ab7ad6541ea95ec6d6b3885
OlenaPustova <i.am.elena.p@gmail.com>
2022-06-17 20:49:23 +0300

    start files

---
+++ contacts.js
@@ -0,0 +1,5 @@
+const fs = require('fs').promises;
+const path  = require('path').promises;
+
+console.log(fs);
+console.log(path);
---
+++ index.js
@@ -0,0 +1,11 @@
+global.test = 20;
+
+// console.log(__filename);
+// console.log(__dirname);
+// console.log(test);
+
+const fs = require('fs').promises;
+const path  = require('path').promises;
+
+console.log(fs);
+// console.log(path);
