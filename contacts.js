8ae1ac5b2646a4d13cca261535e55cc5b9d24288
OlenaPustova <i.am.elena.p@gmail.com>
2022-06-22 16:23:29 +0300

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -1,5 +1,39 @@
-const fs = require('fs').promises;
-const path  = require('path').promises;
+const fs = require('fs');
+const path  = require('path');
 
-console.log(fs);
-console.log(path);
+// console.log(fs);
+
+const contactsPath = path.resolve('db/contacts.json');
+
+fs.readFile(contactsPath, 'utf-8', (error, data) => {
+  if (error) {
+    console.error(error);
+  }
+  console.log(data);
+} )
+
+// console.log(contactsPath);
+
+// // TODO: задокументувати кожну функцію
+// function listContacts() {
+//   // ...твій код
+// }
+
+// function getContactById(contactId) {
+//   // ...твій код
+// }
+
+// function removeContact(contactId) {
+//   // ...твій код
+// }
+
+// function addContact(name, email, phone) {
+//   // ...твій код
+// }
+
+// module.exports = {
+//   listContacts,
+//   getContactById,
+//   removeContact,
+//   addContact
+// }
