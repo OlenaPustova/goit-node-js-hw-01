0292ea6882054b0643c29d45e909132380ee0fe5
OlenaPustova <i.am.elena.p@gmail.com>
2022-06-23 20:44:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const {  listContacts,
+const {
+  listContacts,
   getContactById,
   removeContact,
-  addContact } = require("./contacts");
+  addContact,
+} = require("./contacts");
 
+const argv = require("yargs").argv;
 
-  const argv = require("yargs").argv;
-
-// TODO: рефакторить
 function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      // ...
+      listContacts();
       break;
 
     case "get":
-      // ... id
+      getContactById(id);
       break;
 
     case "add":
-      // ... name email phone
+      addContact(name, email, phone);
       break;
 
     case "remove":
-      // ... id
+      removeContact(id);
       break;
 
     default:
