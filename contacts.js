4615080cfd5171eaec810d9c2ae44b406353ac16
OlenaPustova <i.am.elena.p@gmail.com>
2022-06-23 20:39:44 +0300

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -1,39 +1,124 @@
-const fs = require('fs');
-const path  = require('path');
+const path = require("path");
+const fs = require("fs").promises;
+const uniqid = require('uniqid'); ;
 
-// console.log(fs);
 
-const contactsPath = path.resolve('db/contacts.json');
+const contactsPath = path.resolve("db/contacts.json");
 
-fs.readFile(contactsPath, 'utf-8', (error, data) => {
-  if (error) {
-    console.error(error);
-  }
-  console.log(data);
-} )
+// fs.readFile(contactsPath, 'utf-8', (error, data) => {
+//   if (error) {
+//     console.error(error);
+//   }
+//   console.log(data);
+// } )
 
 // console.log(contactsPath);
 
-// // TODO: задокументувати кожну функцію
+
+// async () => {
+//   try {
+//     const data = await fs.readFile(contactsPath, "utf-8");
+//     const contacts = JSON.parse(data.toString());
+//     console.table(contacts);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
+// TODO: задокументувати кожну функцію
+
 // function listContacts() {
-//   // ...твій код
+//   fs.readFile(contactsPath)
+//     .then((data) => {
+//       const contacts = JSON.parse(data.toString());
+//       console.table(contacts);
+//     })
+//     .catch((error) => console.error(error));
 // }
+
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data.toString());
+    console.table(contacts);
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 // function getContactById(contactId) {
-//   // ...твій код
+//   fs.readFile(contactsPath)
+//     .then((data) => {
+//       const contacts = JSON.parse(data.toString());
+//       const contact = contacts.find((contact) => contact.id == contactId);
+//       console.log(contact);
+//       console.log(fs);
+//     })
+//     .catch((error) => console.error(error));
 // }
+
+async function getContactById(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data.toString());
+    const contact = contacts.find((contact) => contact.id == contactId);
+    console.log(contact);
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 // function removeContact(contactId) {
-//   // ...твій код
+//   fs.readFile(contactsPath)
+//     .then((data) => {
+//       const contacts = JSON.parse(data.toString());
+//       const newData = contacts.filter(
+//         (contact) => Number(contact.id) !== contactId
+//       );
+
+//       fs.writeFile(contactsPath, newData, "utf-8");
+//       // console.log(newData);
+//     })
+//     .catch((error) => console.error(error));
+
+//   // console.log("remove contact " + contactId);
 // }
+
+async function removeContact(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data.toString());
+    const newData = JSON.stringify(
+      contacts.filter((contact) => Number(contact.id) !== contactId),
+      null,
+      2
+    );
+    await fs.writeFile("db/contacts2.json", newData);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 // function addContact(name, email, phone) {
-//   // ...твій код
+//   console.log("add contact " + name + email + phone);
 // }
 
-// module.exports = {
-//   listContacts,
-//   getContactById,
-//   removeContact,
-//   addContact
-// }
+async function addContact(name, email, phone) {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data.toString());
+    const contact = { id: uniqid(), name, email, phone };
+    const newData = JSON.stringify([...contacts, contact], null, 2);
+    // console.log(newData);
+    await fs.writeFile("db/contacts2.json", newData);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
